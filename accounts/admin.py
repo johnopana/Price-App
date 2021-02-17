6c4acb56520bda645ad3d716b5fea4533b048174
johnopana <johnopana2016@gmail.com>
2021-02-17 20:08:58 +0300

    added files

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Customer
+from .models import Product
+from. models import Order
 
 # Register your models here.
+
+admin. site .register(Customer)
+admin. site. register(Product)
+admin. site .register(Order)
