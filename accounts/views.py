1afa66d8da2d5f5b398c5b5408f676e0cb2a1604
johnopana <johnopana2016@gmail.com>
2021-02-22 14:39:02 +0300

    uploaded images

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
 from django .http import HttpResponse
-
 from . models import *
 
 
 
 def home(request):
-    orders = Orders .objects . all()
-    customers = Customer.objects. all()
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    total_customers = customers.count()
+    total_orders = orders.count()
+
+    delivered = Orders.filter(status='Delivered')
+    # similar_actions = Action.objects.filter(created__gte=last_minute, user_id=user.id, verb=verb)
+
+    pending = orders.filter(status='Pending').count()
+
+    context = {'customers':customers , 'orders':orders,
+    'total_orders':total_orders,'delivered':delivered, 'pending':pending }
     return render (request , 'accounts/Dashboard.html', context)
 
-    context = {'customers':customers , 'orders':orders}
 
 def products(request):
     products = Product.objects.all()
